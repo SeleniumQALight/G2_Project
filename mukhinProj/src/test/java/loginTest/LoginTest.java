58494faf2b35bddb4592fa7907a728c43a750c49
Edemdm1 <edemdm1@gmail.com>
2021-07-20 17:07:01 +0300

    Auto test-Login with invalid username and valid password

--- mukhinProj/src/test/java/loginTest/LoginTest.java
+++ mukhinProj/src/test/java/loginTest/LoginTest.java
@@ -12,6 +12,8 @@ import java.util.concurrent.TimeUnit;
 public class LoginTest {
     WebDriver webDriver;
 
+
+//    Login with valid username and password
     @Test
     public void validLogin(){
         File fileFF = new File("./src/drivers/91-1/chromedriver.exe");
@@ -42,6 +44,87 @@ public class LoginTest {
     }
 
     private boolean isButtonSignOutVisible() {
+        try {
+            return webDriver.findElement(By.xpath(".//button[text()='Sign Out']")).isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+
+    }
+
+//    Login with invalid username and valid password
+    @Test
+    public void invalidLogin(){
+        File fileFF = new File("./src/drivers/91-1/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("Site was opened");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder = 'Username']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder = 'Username']")).sendKeys("auto1");
+        System.out.println("'auto1' was inputted");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).sendKeys("123456qwerty");
+        System.out.println(("pass was inputted"));
+
+        
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("Button was clicked");
+
+        Assert.assertFalse("Button SignOut is  displayed", isButtonSignOutNotVisible());
+
+        webDriver.quit();
+
+    }
+
+    private boolean isButtonSignOutNotVisible() {
+        try {
+            return !webDriver.findElement(By.xpath(".//button[text()='Sign Out']")).isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+
+    }
+//    Alternative version
+//Login with invalid username and valid password
+
+    @Test
+    public void invalidLoginAlterbativeVersion(){
+        File fileFF = new File("./src/drivers/91-1/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("Site was opened");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder = 'Username']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder = 'Username']")).sendKeys("auto1");
+        System.out.println("'auto1' was inputted");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).sendKeys("123456qwerty");
+        System.out.println(("pass was inputted"));
+
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("Button was clicked");
+
+        Assert.assertTrue("Button SignOut is not displayed", !isButtonSignOutVisibleOnPage());
+
+        webDriver.quit();
+
+    }
+
+    private boolean isButtonSignOutVisibleOnPage() {
         try {
             return webDriver.findElement(By.xpath(".//button[text()='Sign Out']")).isDisplayed();
         }catch (Exception e){
