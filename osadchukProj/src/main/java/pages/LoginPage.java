ef68ffbfa0c3d86c642ad9a43f948da371eab98f
LiudmylaO <luda@eprostir.com>
2021-07-22 21:38:15 +0300

    second

--- osadchukProj/src/main/java/pages/LoginPage.java
+++ osadchukProj/src/main/java/pages/LoginPage.java
@@ -4,10 +4,20 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends ParentPage{
+    @FindBy(xpath = ".//input[@placeholder='Username']")
+    private WebElement inputLogin;
+
+    @FindBy(xpath = ".//input[@placeholder='Password']")
+    private WebElement inputPassWord;
+
+    @FindBy(xpath = ".//button[text()='Sign In']")
+    private WebElement buttonSignIn;
+
     public LoginPage(WebDriver webDriver) {
-                super(webDriver);
+        super(webDriver);
     }
 
     public void openLoginPage(){
@@ -21,14 +31,26 @@ public class LoginPage extends ParentPage{
     }
 
     public void enterLoginInSignIn(String login) {
-        try {
-            WebElement element = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
-            element.clear();
-            element.sendKeys(login);
-            logger.info(login + " was inputted in SignIn input login");
-        }catch (Exception e){
-            logger.error("Can not work with element" + e);
-            Assert.fail("Can not work with element" + e);
-        }
+//        try{
+////            WebElement element = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
+//            inputLogin.clear();
+//            inputLogin.sendKeys(login);
+//            logger.info(login + " was inputted in SignIn input login");
+//
+//        }catch (Exception e){
+//            logger.error("Can not work with element" + e);
+//            Assert.fail("Can not work with element" + e);
+//        }
+        enterTextToElement(inputLogin, login);
+    }
+
+    public void enterPassWordInSignIn(String passWord) {
+        enterTextToElement(inputPassWord, passWord);
+    }
+
+    public void clickOnButtonSignIn() {
+        clickOnElement(buttonSignIn);
+
+
     }
 }
