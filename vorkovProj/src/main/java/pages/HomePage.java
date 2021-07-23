97466f97f76dff3d3bf9fd79a584e17b37291e28
yvorkoff33 <81635389+yvorkoff33@users.noreply.github.com>
2021-07-23 12:49:23 +0300

    created invalid login test.  HW 3 part 1 of 2

--- vorkovProj/src/main/java/pages/HomePage.java
+++ vorkovProj/src/main/java/pages/HomePage.java
@@ -13,7 +13,21 @@ public class HomePage extends ParentPage {
     @FindBy(xpath = ".//button[text()='Sign Out']")
     private WebElement buttonSignOut;
 
+    @FindBy(xpath = ".//button[text()='Sign In']")
+    WebElement buttonSignIn;
+
+    @FindBy(xpath = ".//div[contains(text(),'Invalid username / password')]")
+    WebElement alertInvalidLogin;
+
     public boolean isButtonSignOutPresent() {
         return isElementPresent(buttonSignOut);
+    }
+
+    public boolean isButtonSignInPresent() {
+        return isElementPresent(buttonSignIn);
+    }
+
+    public boolean isAlertPresent() {
+        return isElementPresent(alertInvalidLogin);
     }
 }
--- vorkovProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ vorkovProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -14,7 +14,18 @@ public class LoginTestWithPageObject extends BaseTest {
         loginPage.clickOnButtonSignIn();
 
         checkExpectedResult("Button Sign Out isn't visible", homePage.isButtonSignOutPresent(), true);
+    }
 
+    @Test
+    public void invalidLogin(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginInSignIn("auto");
+        loginPage.enterPasswordInSignIn("123");
+        loginPage.clickOnButtonSignIn();
+
+        checkExpectedResult("Button Sign Out is visible, but shouldn't", homePage.isButtonSignOutPresent(), false);
+        checkExpectedResult("Button Sign In is visible", homePage.isButtonSignInPresent(), true);
+        checkExpectedResult("Alert invalid login present", homePage.isAlertPresent(), true);
 
     }
 }
