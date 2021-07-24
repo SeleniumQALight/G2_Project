ee678159287a906378155f7616709b1e2e55aa3e
Pavel Canayev <Pavel.Canayev@upc.ua>
2021-07-24 23:51:42 +0300

    HomeWork#3 (invalidLogin Test, validationOnRegistration Test)

--- kanaievProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ kanaievProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -5,12 +5,44 @@ import org.junit.Test;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
-    public void validLogin(){
+    public void validLogin() {
         loginPage.openLoginPage();
         loginPage.enterLoginInSignIn("auto");
         loginPage.enterPasswordInSignIn("123456qwerty");
         loginPage.clickOnButtonSignIn();
 
-        checkExpectedResult("Button SingOut is not visible", homePage.isButtonSignOutPresent(),true);
+        checkExpectedResult("Button SingOut is not visible",
+                homePage.isButtonSignOutPresent(), true);
+    }
+
+    @Test
+    public void invalidLogin() {
+        loginPage.openLoginPage();
+        loginPage.enterLoginInSignIn("auto");
+        loginPage.enterPasswordInSignIn("123");
+        loginPage.clickOnButtonSignIn();
+
+        checkExpectedResult("Button SingOut is visible",
+                homePage.isButtonSignOutPresent(), false);
+        checkExpectedResult("Button SingIn is not visible",
+                loginPage.isButtonSignInPresent(), true);
+        checkExpectedResult("Alert Invalid Sign In login is not visible",
+                loginPage.isAlertInvalidSingInPresent(), true);
+    }
+
+    @Test
+    public void validationOnRegistration(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginInRegLogin("tr");
+        loginPage.enterEmailInRegEmail("test.com");
+        loginPage.enterPasswordInRegPassword("123");
+        loginPage.clickOnButtonSignUp();
+
+        checkExpectedResult("Alert invalid Sing Up login is not visible",
+                loginPage.isAlertInvalidSingUpLoginPresent(), true);
+        checkExpectedResult("Alert invalid Sing Up email is not visible",
+                loginPage.isAlertInvalidSingUpEmailPresent(), true);
+        checkExpectedResult("Alert invalid Sing Up password is not visible",
+                loginPage.isAlertInvalidSingUpPasswordPresent(), true);
     }
 }
