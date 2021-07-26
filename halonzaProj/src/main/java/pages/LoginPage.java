87be2f0443b26005af8caceb431b30d16bcedefd
qanaha <malvanatalya@ukr.net>
2021-07-26 14:28:51 +0300

    invalidLogin test, SignUp fields alerts test

--- halonzaProj/src/main/java/pages/LoginPage.java
+++ halonzaProj/src/main/java/pages/LoginPage.java
@@ -15,6 +15,30 @@ public class LoginPage extends ParentPage{
     @FindBy(xpath = ".//button[text()='Sign In']")
     private WebElement buttonSignIn;
 
+    @FindBy(xpath = ".//div[text()='Invalid username / password']")
+    private WebElement alertSignIn;
+
+    @FindBy(xpath = ".//input[@id='username-register']")
+    private WebElement inputLoginSignUpForm;
+
+    @FindBy(xpath = ".//input[@id='email-register']")
+    private WebElement inputEmailSignUpForm;
+
+    @FindBy(xpath = ".//input[@id='password-register']")
+    private WebElement inputPasswordSignUpForm;
+
+    @FindBy(xpath = ".//button[text()='Sign up for OurApp']")
+    private WebElement buttonSignUp;
+
+    @FindBy(xpath = ".//div[contains(text(), 'Username must be at least 3 characters')]")
+    private WebElement usernameSignUpAlert;
+
+    @FindBy(xpath = ".//div[contains(text(), 'You must provide a valid email address')]")
+    private WebElement emailSignUpAlert;
+
+    @FindBy(xpath = ".//div[contains(text(), 'Password must be at least 12 characters')]")
+    private WebElement passwordSignUpAlert;
+
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -46,7 +70,60 @@ public class LoginPage extends ParentPage{
         enterTextToElement(inputPassWord, passWord);
     }
 
+
     public void clickOnButtonSignIn() {
         clickOnElement(buttonSignIn);
     }
+
+
+    public void fillLoginFormAndSubmit(String login, String passWord){
+        enterLoginInSignIn(login);
+        enterPassWordInSignIn(passWord);
+        clickOnButtonSignIn();
+    }
+
+
+    public boolean isButtonSignInPresent(){
+        return isElementPresent(buttonSignIn);
+    }
+
+    public boolean isSignInAlertPresent(){
+        return isElementPresent(alertSignIn);
+    }
+
+    public void enterLoginInSignUp(String login) {
+        enterTextToElement(inputLoginSignUpForm, login);
+    }
+
+    public void enterEmailInSignUp(String email) {
+        enterTextToElement(inputEmailSignUpForm, email);
+    }
+
+    public void enterPassWordInSignUp(String passWord) {
+        enterTextToElement(inputPasswordSignUpForm, passWord);
+    }
+
+    public void clickOnButtonSignUp() {
+        clickOnElement(buttonSignUp);
+    }
+
+    public void fillSignUpFormAndSubmit(String login, String email, String passWord){
+        enterLoginInSignUp(login);
+        enterEmailInSignUp(email);
+        enterPassWordInSignUp(passWord);
+        clickOnButtonSignUp();
+    }
+
+    public boolean isUsernameSignUpAlertPresent(){
+        return isElementPresent(usernameSignUpAlert);
+    }
+
+    public boolean isEmailSignUpAlertPresent(){
+        return isElementPresent(emailSignUpAlert);
+    }
+
+    public boolean isPasswordSignUpAlertPresent(){
+        return isElementPresent(passwordSignUpAlert);
+    }
+
 }
--- halonzaProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ halonzaProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -12,4 +12,22 @@ public class LoginTestWithPageObject extends BaseTest {
         loginPage.clickOnButtonSignIn();
         checkExpectedResult("Button SignOut is not visible", homePage.isButtonSignOutPresent(),true);
     }
+
+    @Test
+    public void invalidLogin(){
+        loginPage.openLoginPage();
+        loginPage.fillLoginFormAndSubmit("auto", "123");
+        checkExpectedResult("Button SignOut is visible", homePage.isButtonSignOutPresent(),false);
+        checkExpectedResult("Button SignIn is not visible", loginPage.isButtonSignInPresent(),true);
+        checkExpectedResult("SignIn alert is not visible", loginPage.isSignInAlertPresent(),true);
+    }
+
+    @Test
+    public void signUpFieldValidationAlerts(){
+        loginPage.openLoginPage();
+        loginPage.fillSignUpFormAndSubmit("tr", "test.com", "123");
+        checkExpectedResult("Invalid username alert is not visible", loginPage.isUsernameSignUpAlertPresent(), true);
+        checkExpectedResult("Invalid email alert is not visible", loginPage.isEmailSignUpAlertPresent(),true);
+        checkExpectedResult("Invalid password alert is not visible", loginPage.isPasswordSignUpAlertPresent(),true);
+    }
 }
