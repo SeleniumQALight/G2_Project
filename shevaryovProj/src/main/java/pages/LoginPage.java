a182c105e9224d669fdfa3c1858528cab961d579
sholm73 <sholm73@gmail.com>
2021-07-27 00:12:19 +0300

    Login page with page object second part by Shevaryov Oleg

--- shevaryovProj/src/main/java/pages/LoginPage.java
+++ shevaryovProj/src/main/java/pages/LoginPage.java
@@ -4,8 +4,21 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 
 public class LoginPage extends ParentPage{
+//    xpath к инпуту с именем пользователя
+    @FindBy(xpath = ".//input[@placeholder='Username']")
+    private WebElement inputLogin;
+//    xpath к инпуту с паролем
+    @FindBy(xpath = ".//input[@placeholder='Password']")
+    private WebElement inputPassWord;
+//    xpath к кнопке Залогинится
+    @FindBy(xpath = ".//button[text()='Sign In']")
+    private WebElement buttonSignIn;
+
+//    конструктор
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -21,15 +34,25 @@ public class LoginPage extends ParentPage{
     }
 
     public void enterLoginInSignIn(String login) {
-        //Находим поле логина и заполняем логином
-        try {
-            WebElement element = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
-            element.clear();
-            element.sendKeys(login);
-            logger.info(login + " was inputted in SignIn input login");
-        }catch (Exception e){
-            logger.error("Can not work with element " + e);
-            Assert.fail("Can not work with element");
-        }
+//        Находим поле логина и заполняем логином
+//        try {
+//             создаем @FindBy вместо этой строки
+//            WebElement element = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
+//            inputLogin.clear();
+//            inputLogin.sendKeys(login);
+//            logger.info(login + " was inputted in SignIn input login");
+//        }catch (Exception e){
+//            logger.error("Can not work with element " + e);
+//            Assert.fail("Can not work with element");
+//        }
+        enterTextToElement(inputLogin, login);
+    }
+
+    public void enterPassWordInSignIn(String passWord) {
+        enterTextToElement(inputPassWord, passWord);
+    }
+
+    public void clickOnButtonInSignIn() {
+        clickOnElement(buttonSignIn);
     }
 }
