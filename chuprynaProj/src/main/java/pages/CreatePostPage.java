0125c307f3f978a1cafa23a60df88c6a6f1096ef
achupryna <a.chuprynka@gmail.com>
2021-07-27 21:26:18 +0300

    [lesson-5] Added test for post creation

---
+++ chuprynaProj/src/main/java/pages/CreatePostPage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CreatePostPage extends ParentPage {
+    //    @FindBy(xpath = ".//input[@name='title']")
+    @FindBy(name = "title")
+    private WebElement inputTitle;
+
+    @FindBy(id = "post-body")
+    private WebElement inputBody;
+
+    @FindBy(xpath = ".//button[text()='Save New Post']")
+    private WebElement buttonSavePost;
+
+    public CreatePostPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public CreatePostPage checkIsInputTitlePresent() {
+        Assert.assertTrue("Input title is not present", isElementPresent(inputTitle));
+        return this;
+    }
+
+    public CreatePostPage enterTextIntoPostTitleInput(String postTitle) {
+        enterTextToElement(inputTitle, postTitle);
+        return this;
+    }
+
+    public CreatePostPage enterTextIntoPostBodyInput(String postBody) {
+        enterTextToElement(inputBody, postBody);
+        return this;
+    }
+
+    public PostPage clickOnButtonSavePost() {
+        clickOnElement(buttonSavePost);
+        return new PostPage(webDriver);
+    }
+}
