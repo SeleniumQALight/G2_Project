1687d28c596e005a76aeb8ee263ab07f3d43dd1c
Oleh Dubishchev <Oleh.Dubishchev@infopulse.com>
2021-07-27 21:25:33 +0300

    Add createPost test

--- dubishchevProj/src/main/java/pages/HomePage.java
+++ dubishchevProj/src/main/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,6 +8,8 @@ import org.openqa.selenium.support.FindBy;
 public class HomePage extends ParentPage {
     @FindBy(xpath = ".//button[text()='Sign Out']")
     private WebElement buttonSignOut;
+    @FindBy(xpath = ".//a[text()='Create Post']")
+    private WebElement buttonCreatePost;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
@@ -17,4 +20,13 @@ public class HomePage extends ParentPage {
     }
 
 
+    public HomePage checkIsButtonSignOutVisible() {
+        Assert.assertTrue("Button SignOut not visible", isButtonSignOutPresent());
+        return this;
+    }
+
+    public CreatePostPage clickOnButtonCreatePost() {
+        clickOnElement(buttonCreatePost);
+        return new CreatePostPage(webDriver);
+    }
 }
