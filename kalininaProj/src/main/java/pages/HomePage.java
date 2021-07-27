d79d882caf9b7082a42c38611a4f2f39d7f3a9fc
Olena Kalinina <allayavna@gmail.com>
2021-07-27 21:26:30 +0300

    changes from 07-27

--- kalininaProj/src/main/java/pages/HomePage.java
+++ kalininaProj/src/main/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,11 +9,27 @@ public class HomePage extends ParentPage{
     @FindBy(xpath = ".//button[text()='Sign Out']")
     private WebElement buttonSignOut;
 
+    @FindBy(xpath = ".//a[text()='Create Post']")
+    private WebElement buttonCreatePost;
+
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public boolean isButtonSignOutPresent(){
         return isElementPresent(buttonSignOut);
+    }
+
+    public HomePage checkIsButtonSignOutVisible(){
+        Assert.assertTrue("Button Sign Out is not displayed",isButtonSignOutPresent());
+        //return new HomePage(webDriver);
+        return this;
+
+    }
+
+    public CreatePostPage clickOnButtonCreatePost() {
+        clickOnElement(buttonCreatePost);
+        return new CreatePostPage(webDriver);
     }
 }
