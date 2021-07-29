ab99dc27fa6395644c52c24fba09ece39fd63480
zulu <>
2021-07-29 20:52:45 +0300

    Test "Creating Post"

--- lavrenkoProj/src/test/java/postsTest/CreatePostTest.java
+++ lavrenkoProj/src/test/java/postsTest/CreatePostTest.java
@@ -7,16 +7,22 @@ import org.junit.Test;
 public class CreatePostTest extends BaseTest {
     final String POST_TITLE = "Illia title" + Util.getDateAndTimeFormatted();
     final String POST_BODY = "Body test";
+
     @Test
-    public void createPost(){
+    public void createPost() {
         loginPage.loginWithValidCred()
                 .checkIsButtonSignOutVisible()
                 .clickOnButtonCreatePost()
                 .checkIsInputTitlePresent()
                 .enterTextIntoInputTitle(POST_TITLE)
                 .enterTextIntoBody("POST BODY")
-        .clickOnSaveButton()
-                ;
+                .clickOnSaveButton()
+                .checkIsButtonDeletePresent()
+                .checkIsSuccesMessagePresent()
+                .checkTextInSuccessMessage("New post successfully created.")
+        .clickOnTheProfileButton()
+        .checkIsPostWasAdded(POST_TITLE)
+        ;
     }
 }
 
