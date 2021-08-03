38731af26e93bf16824af6e899d4724162133122
Olena Kalinina <allayavna@gmail.com>
2021-08-03 21:28:29 +0300

    changes from 08-03

--- kalininaProj/src/main/java/pages/CreatePostPage.java
+++ kalininaProj/src/main/java/pages/CreatePostPage.java
@@ -15,6 +15,9 @@ public class CreatePostPage extends ParentPage{
     @FindBy(xpath = ".//button[text()='Save New Post']")
     private WebElement buttonSave;
 
+    @FindBy(xpath = ".//select[@id='select1']")
+    private WebElement dropDownSelectValue;
+
 
     public CreatePostPage(WebDriver webDriver) {
         super(webDriver);
@@ -37,5 +40,15 @@ public class CreatePostPage extends ParentPage{
     public PostPage clickOnSaveButton() {
         clickOnElement(buttonSave);
         return new PostPage(webDriver);
+    }
+
+    public CreatePostPage selectTextInDropDownSelectValue(String text) {
+        selectTextInDropDown(dropDownSelectValue, text);
+        return this;
+    }
+
+    public CreatePostPage selectValueInDropDownSelectValue(String value) {
+        selectValueInDropDown(dropDownSelectValue, value);
+        return this;
     }
 }
