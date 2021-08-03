88d26be10ca1e9e72a5d1d0b364225c554cdffad
Edemdm1 <edemdm1@gmail.com>
2021-08-03 15:50:02 +0300

    Create post test

---
+++ mukhinProj/src/test/java/postsTest/CreatePost.java
@@ -0,0 +1,24 @@
+package postsTest;
+
+import baseTest.BaseTest;
+import libs.Util;
+import org.junit.Test;
+
+public class CreatePost extends BaseTest {
+    final String POST_TITLE = "DenDeche title of Post" + Util.getDateAndTimeFormatted();
+    @Test
+    public void createPost() {
+        loginPage
+                .loginWithValidCred()
+                .checkIsButtonSignOutVisible()
+                .clickOnButtonCreatePost()
+                .checkIsInputTitlePresent()
+                .enterTextIntoInputTitle(POST_TITLE)
+                .enterTextIntoInputBody("Body text")
+                .clickOnSveButton()
+
+
+                ;
+
+    }
+}
