4054cd1302c27d3e91c4938bcd7e3fc8ecd28b3b
sholm73 <sholm73@gmail.com>
2021-08-05 21:08:36 +0300

    Params by Shevaryov Oleg

--- shevaryovProj/src/test/java/registrationTest/RegistrationTest.java
+++ shevaryovProj/src/test/java/registrationTest/RegistrationTest.java
@@ -1,17 +1,29 @@
 package registrationTest;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+//Запуск тестов с параметрами
+@RunWith(JUnitParamsRunner.class)
 public class RegistrationTest extends BaseTest {
     @Test
-    public void registrationErrors(){
+//    запуск теста с двумя наборами параметров по 4 параметра
+    @Parameters({
+            "12,qqq,345,Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters.",
+            "12,qqq,123456qwerty,Username must be at least 3 characters.;You must provide a valid email address.",
+    })
+//    название теста
+    @TestCaseName("registrationErrors: login = {0}, email={1}, passWord={2}")
+    public void registrationErrors(String login, String email, String passWord, String errors){
         loginPage.openLoginPage();
-        loginPage.enterLoginInRegistration("12")
-                .enterEmailInRegistration("qqq")
-//                .enterPassWordRegistration("123")
-                .checkErrorsMessage("Username must be at least 3 characters.;You must provide a valid email address.")
-
+        loginPage.enterLoginInRegistration(login)
+                .enterEmailInRegistration(email)
+                .enterPassWordRegistration(passWord)
+                .checkErrorsMessage(errors)
         ;
     }
 }
