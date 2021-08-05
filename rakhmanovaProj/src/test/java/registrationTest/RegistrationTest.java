c09269db44f8e3b49b367c60b5e0cddfa2bc3507
Tetiana Rakhmanova <tetiana_rakhmanova@epam.com>
2021-08-05 21:08:31 +0300

    Params

--- rakhmanovaProj/src/test/java/registrationTest/RegistrationTest.java
+++ rakhmanovaProj/src/test/java/registrationTest/RegistrationTest.java
@@ -1,17 +1,28 @@
 package registrationTest;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+
+@RunWith(JUnitParamsRunner.class)
 public class RegistrationTest extends BaseTest {
     @Test
-    public void registrationErrors(){
+    @Parameters({
+            "12,qqq,345,Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters.",
+            "12,qqq,123456qwerty,Username must be at least 3 characters.;You must provide a valid email address."
+    })
+    @TestCaseName("registrationErrors : login = {0}, email = {1}, password = {2}")
+    public void registrationErrors(String login, String email, String password, String errors){
         loginPage.openLoginPage();
         loginPage
-                .enterLoginInRegistration("12")
-                .enterEmailInRegistration("qqq")
-                //.enterPasswordRegistration("345")
-                .checkErrorsMessages("Username must be at least 3 characters.;You must provide a valid email address.")
+                .enterLoginInRegistration(login)
+                .enterEmailInRegistration(password)
+                .enterPasswordRegistration(password)
+                .checkErrorsMessages(errors)
                 ;
 
     }
