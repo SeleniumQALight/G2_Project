099400899580f85ee3e83caa2e620deca8888cdd
Taras Radulenko <Taras_Radulenko@epam.com>
2021-08-05 21:08:24 +0300

    Params

--- overAll/src/test/java/registationTest/RegistrationTest.java
+++ overAll/src/test/java/registationTest/RegistrationTest.java
@@ -1,18 +1,28 @@
 package registationTest;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 
+@RunWith(JUnitParamsRunner.class)
 public class RegistrationTest extends BaseTest {
     @Test
-    public void registrationErrors(){
+    @Parameters({
+            "12,qqq,345,Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters.",
+            "12,qqq,123456qwerty,Username must be at least 3 characters.;You must provide a valid email address."
+    })
+    @TestCaseName("registrationErrors : login = {0}, email = {1}, password = {2}")
+    public void registrationErrors(String login, String email, String passWord, String errors){
         loginPage.openLoginPage();
         loginPage
-                .enterLoginInRegistration("12")
-                .enterEmailInRegistration("qqq")
-//                .enterPassWordRegistration("345")
-               .checkErrorsMessages("Username must be at least 3 characters.;You must provide a valid email address.")
+                .enterLoginInRegistration(login)
+                .enterEmailInRegistration(email)
+                .enterPassWordRegistration(passWord)
+               .checkErrorsMessages(errors)
         ;
     }
 }
