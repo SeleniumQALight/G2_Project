bbf91ae11a45a0ab867b5eef3d1fa2fb09fa1e41
Olena Kalinina <allayavna@gmail.com>
2021-08-10 21:17:35 +0300

    changes from 08-10 / rule

--- kalininaProj/src/test/java/baseTest/BaseTest.java
+++ kalininaProj/src/test/java/baseTest/BaseTest.java
@@ -1,9 +1,13 @@
 package baseTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import libs.Util;
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
@@ -17,9 +21,14 @@ public class BaseTest {
     WebDriver webDriver;
     protected LoginPage loginPage;
     protected HomePage homePage;
+    protected Logger logger = Logger.getLogger(getClass());
+
+    @Rule
+    public TestName testName = new TestName();
 
     @Before
     public void setUp(){
+        logger.info("-------" + testName.getMethodName() + " was started ----------");
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -31,7 +40,9 @@ public class BaseTest {
     @After
     public void tearDown(){
         webDriver.quit();
+        logger.info("-------" + testName.getMethodName() + " was ended ---------" + Util.getDateAndTimeFormated());
     }
+
 
     protected void checkExpectedResult(String message, boolean actualResult, boolean expectedResult){
        // Assert.assertThat(message, actualResult, is(expectedResult));
