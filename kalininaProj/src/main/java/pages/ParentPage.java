a232c3e95707a4eaa27a09f1d1aceb5f577f4001
Olena Kalinina <allayavna@gmail.com>
2021-08-10 21:04:02 +0300

    changes from 08-10 / yandex

--- kalininaProj/src/main/java/pages/ParentPage.java
+++ kalininaProj/src/main/java/pages/ParentPage.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.yandex.qatools.htmlelements.element.TypifiedElement;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
 
 import static org.hamcrest.CoreMatchers.containsString;
 
@@ -23,7 +26,11 @@ public abstract class ParentPage {
 
     public ParentPage(WebDriver webDriver) {
         this.webDriver = webDriver;
-        PageFactory.initElements(webDriver, this);
+       // PageFactory.initElements(webDriver, this); инициалазарует WebElement
+        PageFactory.initElements(  //инициализирует yandexHTML Elements
+                new HtmlElementDecorator(
+                new HtmlElementLocatorFactory(webDriver))
+                ,this);
         webDriverWait10 = new WebDriverWait(webDriver, 10);
         webDriverWait15 = new WebDriverWait(webDriver, 15);
 
@@ -47,18 +54,36 @@ public abstract class ParentPage {
             webDriverWait15.until(ExpectedConditions.visibilityOf(webElement));
             webElement.clear();
             webElement.sendKeys(text);
-            logger.info("'" + text + "' was inputted in element ");
+            logger.info("'" + text + "' was inputted in element " + getElementName(webElement));
         }catch (Exception  e){
             writeErrorAndStopTest(e);
 
         }
     }
 
+    private String getElementName(WebElement webElement) {
+        String elementName = "";
+        if (webElement instanceof TypifiedElement){
+            elementName = " '" + ((TypifiedElement) webElement).getName() + " '";
+        }
+        return elementName;
+    }
+
     protected void clickOnElement(WebElement webElement){
         try {
             webDriverWait10.until(ExpectedConditions.elementToBeClickable(webElement));
             webElement.click();
-            logger.info("Element was clicked");
+            logger.info(getElementName(webElement)  + " Element was clicked");
+        }catch (Exception e){
+            writeErrorAndStopTest(e);
+        }
+    }
+
+    protected void clickOnElement(WebElement webElement, String elementName){
+        try {
+            webDriverWait10.until(ExpectedConditions.elementToBeClickable(webElement));
+            webElement.click();
+            logger.info(elementName + " Element was clicked");
         }catch (Exception e){
             writeErrorAndStopTest(e);
         }
@@ -68,13 +93,13 @@ public abstract class ParentPage {
         try{
             boolean state = webElement.isDisplayed();
             if (state) {
-                logger.info("Element present ");
+                logger.info(getElementName(webElement) + " Element present ");
 
             } else {
-                logger.info("Element is not present");
+                logger.info(getElementName(webElement) + " Element is not present");
             } return state;
         }catch (Exception e) {
-            logger.info("Element is not present");
+            logger.info(getElementName(webElement) + " Element is not present");
             return false;
         }
     }
@@ -83,7 +108,7 @@ public abstract class ParentPage {
         try{
             Select select = new Select(dropDown); // класс по работе с  дропдауном
             select.selectByVisibleText(text);
-            logger.info("'" + text + "' was selected  in DropDown");
+            logger.info("'" + text + "' was selected  in DropDown" + getElementName(dropDown));
 
         }catch (Exception e){
             writeErrorAndStopTest(e);
@@ -94,7 +119,7 @@ public abstract class ParentPage {
         try{
             Select select = new Select(dropDown); // класс по работе с  дропдауном
             select.selectByValue(value);
-            logger.info("'" + value + "' was selected  in DropDown");
+            logger.info("'" + value + "' was selected  in DropDown" + getElementName(dropDown));
 
         }catch (Exception e){
             writeErrorAndStopTest(e);
--- kalininaProj/src/main/java/pages/PostPage.java
+++ kalininaProj/src/main/java/pages/PostPage.java
@@ -4,16 +4,18 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.htmlelements.element.Button;
+import ru.yandex.qatools.htmlelements.element.TextInput;
 
 public class PostPage extends ParentPage{
     @FindBy(xpath = ".//button[@data-original-title='Delete']")
-    WebElement buttonDelete;
+    Button buttonDelete;
 
     @FindBy(xpath = ".//*[@class='alert alert-success text-center']")
-    private WebElement successMessageElement;
+    private TextInput successMessageElement;
 
     @FindBy(xpath = ".//img[@data-original-title='My Profile']")
-    private WebElement buttonProfile;
+    private Button buttonProfile;
 
     public PostPage(WebDriver webDriver) {
         super(webDriver);
