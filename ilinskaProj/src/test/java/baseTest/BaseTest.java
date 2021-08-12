be2d171851bbbdd3b7df7566248a762b6285a7a2
Lera <valerinskaya9972@gmail.com>
2021-08-12 21:20:09 +0300

    Smoke tests

--- ilinskaProj/src/test/java/baseTest/BaseTest.java
+++ ilinskaProj/src/test/java/baseTest/BaseTest.java
@@ -9,37 +9,62 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.HomePage;
 import pages.LoginPage;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver webDriver;
-   protected LoginPage loginPage;
-   protected HomePage homePage;
-   protected Logger logger= Logger.getLogger(getClass());
-   @Rule
-   public TestName testName= new TestName();
+    protected LoginPage loginPage;
+    protected HomePage homePage;
+    protected Logger logger = Logger.getLogger(getClass());
+    @Rule
+    public TestName testName = new TestName();
+
     @Before
-    public  void setUp(){
-        logger.info("------"+testName.getMethodName()+"was started ---------------------");
-   WebDriverManager.chromedriver().setup();
-        webDriver= new ChromeDriver();
+    public void setUp() {
+        logger.info("------" + testName.getMethodName() + "was started ---------------------");
+        webDriver=initDriver();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
-        loginPage= new LoginPage(webDriver);
-        homePage= new HomePage(webDriver);
+        loginPage = new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
     }
+
     @After
-    public  void tearDown(){
+    public void tearDown() {
         webDriver.quit();
-        logger.info("------"+testName.getMethodName()+"was finished  ---------------------");
+        logger.info("------" + testName.getMethodName() + "was finished  ---------------------");
 
 
     }
-    protected void checkExpectedResult(String message,boolean actualResult,boolean expectedResult){
-        Assert.assertEquals(message,expectedResult,actualResult);
+
+    protected void checkExpectedResult(String message, boolean actualResult, boolean expectedResult) {
+        Assert.assertEquals(message, expectedResult, actualResult);
 
     }
 
+    private WebDriver initDriver() {
+        String browser = System.getProperty("browser");
+        if ((browser == null) || browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")){
+          WebDriverManager.firefoxdriver().setup();
+          webDriver= new FirefoxDriver();
+        }
+        else if ("ie".equalsIgnoreCase(browser)) {
+            //WebDriverManager.iedriver().setup();
+
+            // in most cases 32bit version is needed
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+        }
+
+        return webDriver;
+    }
 }
+
+
