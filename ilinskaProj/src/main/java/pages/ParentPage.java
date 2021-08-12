8d6477a6f3512b0cb41379d544493e3ce99c3eaa
Lera <valerinskaya9972@gmail.com>
2021-08-12 21:45:00 +0300

    Browser

--- ilinskaProj/src/main/java/pages/ParentPage.java
+++ ilinskaProj/src/main/java/pages/ParentPage.java
@@ -2,12 +2,17 @@ package pages;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import ru.yandex.qatools.htmlelements.element.TypifiedElement;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
+
+import java.util.ArrayList;
 
 public class ParentPage {
     Logger logger = Logger.getLogger(getClass());
@@ -19,7 +24,7 @@ public class ParentPage {
         PageFactory.initElements(
                 new HtmlElementDecorator(
                         new HtmlElementLocatorFactory(webDriver))
-                ,this);// иметь возможность чтобі работать с елементами яндексс
+                , this);// иметь возможность чтобі работать с елементами яндексс
     }
 
     protected void enterTextToElement(WebElement webElement, String text) {
@@ -27,16 +32,16 @@ public class ParentPage {
 
             webElement.clear();
             webElement.sendKeys(text);
-            logger.info("'" + text + "'was inputted in element"+getElementName(webElement));
+            logger.info("'" + text + "'was inputted in element" + getElementName(webElement));
         } catch (Exception e) {
             writeErrorAndStopTest(e);
         }
     }
 
     private String getElementName(WebElement webElement) {
-        String elementName="";
-        if(webElement instanceof TypifiedElement){
-            elementName=" '"+((TypifiedElement)webElement).getName()+" '";
+        String elementName = "";
+        if (webElement instanceof TypifiedElement) {
+            elementName = " '" + ((TypifiedElement) webElement).getName() + " '";
         }
         return elementName;
 
@@ -45,16 +50,17 @@ public class ParentPage {
     public void clickOnElement(WebElement webElement) {
         try {
             webElement.click();
-            logger.info(getElementName(webElement)+" Element was clicked");
+            logger.info(getElementName(webElement) + " Element was clicked");
         } catch (Exception e) {
             writeErrorAndStopTest(e);
         }
 
     }
-    public void clickOnElement(WebElement webElement,String elementName) {
+
+    public void clickOnElement(WebElement webElement, String elementName) {
         try {
             webElement.click();
-            logger.info(elementName+" Element was clicked");
+            logger.info(elementName + " Element was clicked");
         } catch (Exception e) {
             writeErrorAndStopTest(e);
         }
@@ -63,9 +69,9 @@ public class ParentPage {
 
     protected boolean isElementPresent(WebElement webElement) {
         try {
-            boolean state= webElement.isDisplayed();
+            boolean state = webElement.isDisplayed();
             if (state) {
-                logger.info(getElementName(webElement)+" Element state");
+                logger.info(getElementName(webElement) + " Element state");
             } else {
                 logger.info("Element is not present");
             }
@@ -74,6 +80,29 @@ public class ParentPage {
             logger.info("Element is not present");
             return false;
         }
+    }
+
+    public void usersPressesKeyEnterTime(int numberOfTimes) {
+        Actions actions = new Actions(webDriver);
+        for (int i = 0; i < numberOfTimes; i++) {
+            actions.sendKeys(Keys.ENTER).build().perform();
+
+        }
+    }
+
+    public void userOpensNewTab() {
+        ((JavascriptExecutor) webDriver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+        webDriver.switchTo().window(tabs.get(1));
+    }
+
+    public void usersPressesKeyTabTime(int numberOfTimes) {
+        Actions actions = new Actions(webDriver);
+        for (int i = 0; i < numberOfTimes; i++) {
+            actions.sendKeys(Keys.TAB).build().perform();
+
+        }
+
     }
         private void writeErrorAndStopTest (Exception e){
             logger.error("Сan not work with element" + e);
