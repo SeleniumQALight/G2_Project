1eadfd15063c179b85865b44d605c382f2bc4d44
Hanna.Gupalo <gupanka28@gmail.com>
2021-08-17 21:08:18 +0300

    Excel

---
+++ gupaloProj/src/test/java/registrationTest/RegistrationTestWithExcel.java
@@ -0,0 +1,49 @@
+package registrationTest;
+
+import baseTest.BaseTest;
+import categories.SmokeTestFilter;
+
+
+
+import libs.SpreadsheetData;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import static pages.ParentPage.configProperties;
+
+@RunWith(Parameterized.class)
+@Category(SmokeTestFilter.class)
+public class RegistrationTestWithExcel extends BaseTest {
+    private String login, email, password, errors;
+
+    public RegistrationTestWithExcel(String login, String email, String password, String errors) {
+        this.login = login;
+        this.email = email;
+        this.password = password;
+        this.errors = errors;
+    }
+
+    @Parameterized.Parameters
+    public static Collection testData() throws IOException {
+        InputStream inputStream = new FileInputStream(configProperties.DATA_FILE_PATH() + "testDataSuit.xls");
+        return new SpreadsheetData(inputStream, "InvalidLogOn").getData();
+    }
+
+    @Test
+    public void checkErrorsMessages() {
+        loginPage.openLoginPage();
+        loginPage
+                .enterLoginInRegForm(login)
+                .enterEmailInRegForm(email)
+                .enterPasswordInRegForm(password)
+                .checkErrors(errors);
+
+    }
+}
