177445a412f5bf028bc5e35fa4fa12f627df003d
xomaua <kovalnick@gmail.com>
2021-08-17 21:08:10 +0300

    Excel

--- kovalProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ kovalProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -2,9 +2,14 @@ package loginTest;
 
 import baseTest.BaseTest;
 import categories.SmokeTestFilter;
+import libs.ExcelDriver;
 import libs.TestData;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import pages.ParentPage;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Category(SmokeTestFilter.class)
@@ -27,5 +32,18 @@ public class LoginTestWithPageObject extends BaseTest {
 
         checkExpectedResult("Button SignOut is not visible", homePage.isButtonSignOutPresent() ,true);
     }
+
+    @Test
+    public void validLoginWithExcel() throws IOException {
+        Map<String, String> dataForValidLogin = ExcelDriver.getData(ParentPage.configProperties.DATA_FILE(), "validLogOn");
+        loginPage.openLoginPage();
+        loginPage.enterLoginInSignIn(dataForValidLogin.get("login"));
+        loginPage.enterPasswordInSignIn(dataForValidLogin.get("pass"));
+        loginPage.clickOnButtonSignIn();
+
+        checkExpectedResult("Button SignOut is not visible", homePage.isButtonSignOutPresent() ,true);
+    }
+
+
 
 }
