10e3488e9ac3feba95525803c0de316da5fe7d56
zulu <>
2021-09-06 23:37:58 +0300

    Allure

--- lavrenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ lavrenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -2,8 +2,10 @@ package loginTest;
 
 import baseTest.BaseTest;
 import categories.SmokeTestFilter;
+import io.qameta.allure.*;
 import libs.ExcelDriver;
 import libs.TestData;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import pages.ParentPage;
@@ -12,13 +14,21 @@ import java.io.IOException;
 import java.util.Map;
 
 import static pages.ParentPage.configProperties;
-
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class LoginTestWithPageObject extends BaseTest {
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
     @Category(SmokeTestFilter.class)
     @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Base support for bdd annotations")
     public void validLogin() {
         loginPage.openLoginPage();
-        loginPage.enterLoginInSignIn(TestData.VALID_LOGIN);
+        loginPage.enterLoginInSignIn(TestData.VALID_LOGIN+"123");
         loginPage.enterPasswordInSignIn(TestData.VALID_PASSWORD);
         loginPage.clickOnButtonSignIn();
 
@@ -26,6 +36,7 @@ public class LoginTestWithPageObject extends BaseTest {
     }
 
     @Category(SmokeTestFilter.class)
+//    @Ignore
     @Test
     public void validLoginWithExcel() throws IOException {
         Map<String,String> dataForValidLogin = ExcelDriver.getData(configProperties.DATA_FILE(),"validLogOn");
