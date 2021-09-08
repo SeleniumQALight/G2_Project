0c9efaa6e483677f0204997443687d9dabc955ae
Taras Radulenko <Taras_Radulenko@epam.com>
2021-09-08 22:48:43 +0300

    lasarevaProj was added

---
+++ lazarevaProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -0,0 +1,56 @@
+package loginTest;
+
+import static pages.ParentPage.configProperties;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import baseTest.BaseTest;
+import categories.SmokeTestFilter;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import libs.ExcelDriver;
+import libs.TestData;
+
+@Epic("Allure examples")
+@Feature("Junit 4 support")
+public class LoginTestWithPageObject extends BaseTest {
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Base support for bdd annotations")
+    @Category(SmokeTestFilter.class)
+    public void validLogin(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginInSignIn(TestData.VALID_LOGIN);
+        loginPage.enterPassWordInSignIn(TestData.VALID_PASSWORD);
+        loginPage.clickOnButtonSignIn();
+
+        checkExpectedResult("Button SignOut is not visible", homePage.isButtonSignOutPresent() , true);
+    }
+
+    @Test
+    @Category(SmokeTestFilter.class)
+    public void validLoginWithExcel() throws IOException {
+        Map<String, String> dataForValidLogin = ExcelDriver.getData(configProperties.DATA_FILE(),"validLogOn");
+        loginPage.openLoginPage();
+        loginPage.enterLoginInSignIn(dataForValidLogin.get("login"));
+        loginPage.enterPassWordInSignIn(dataForValidLogin.get("pass"));
+        loginPage.clickOnButtonSignIn();
+
+        checkExpectedResult("Button SignOut is not visible", homePage.isButtonSignOutPresent() , true);
+    }
+}
