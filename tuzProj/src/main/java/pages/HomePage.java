a50725e17f97146bbe45ecdd912f1d8456e8918e
Taras Radulenko <Taras_Radulenko@epam.com>
2021-09-08 22:41:53 +0300

    tuzProj was added

---
+++ tuzProj/src/main/java/pages/HomePage.java
@@ -0,0 +1,62 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import io.qameta.allure.Step;
+import ru.yandex.qatools.htmlelements.element.Button;
+
+public class HomePage extends ParentPage{
+    @FindBy(xpath = ".//img[@data-original-title='My Profile']")
+    private Button buttonProfile;
+
+    @FindBy(xpath = ".//button[text()='Sign Out']")
+    private Button buttonSignOut;
+
+    @FindBy(xpath = ".//a[text()='Create Post']")
+    private Button buttonCreatePost;
+
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @Override
+    String getRelativeUrl() {
+        return "/";
+    }
+
+    public HomePage checkIsRedirectOnHomePage(){
+        checkUrl();
+        checkIsButtonSignOutVisible();
+        return this;
+    }
+    @Step
+    public boolean isButtonSignOutPresent(){
+        return isElementPresent(buttonSignOut);
+    }
+    @Step
+    public HomePage checkIsButtonSignOutVisible() {
+        Assert.assertTrue("Button Sign Out is not displayed", isButtonSignOutPresent());
+        return this;
+    }
+    @Step
+    public CreatePostPage clickOnButtonCreatePost() {
+        clickOnElement(buttonCreatePost);
+        return new CreatePostPage(webDriver);
+    }
+    @Step
+    public HomePage openHomePage() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.openLoginPage();
+        if (!isButtonSignOutPresent()){
+            loginPage.loginWithValidCred();
+        }
+        return this;
+    }
+    @Step
+    public ProfilePage clickOnButtonProfile(){
+        clickOnElement(buttonProfile);
+        return new ProfilePage(webDriver);
+    }
+}
