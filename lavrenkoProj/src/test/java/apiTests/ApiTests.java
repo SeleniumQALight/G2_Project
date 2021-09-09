e4dc430c311d865a6c86f5a950e84646ad3fa3dc
zulu <>
2021-09-09 21:35:17 +0300

    Api Test

---
+++ lavrenkoProj/src/test/java/apiTests/ApiTests.java
@@ -0,0 +1,24 @@
+package apiTests;
+
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static api.Endpoints.POST_BY_USER;
+import static io.restassured.RestAssured.given;
+
+public class ApiTests {
+    final String USER_NAME = "autoapi";
+
+    @Test
+    public void getAllPostsByUser() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+        .when()
+                .get(POST_BY_USER, USER_NAME)
+        .then()
+                .statusCode(200)
+                .log().all();
+    }
+}
