f13a21e06cecd292dae5a3e0399f96b8dba0d1e8
valery772 <valerinskaya9972@gmail.com>
2021-09-09 21:35:42 +0300

    09-09 ilinska API test

---
+++ ilinskaProj/src/test/java/ApiTest/ApiTest.java
@@ -0,0 +1,25 @@
+package ApiTest;
+
+import api.EndPoints;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Test;
+
+import static api.EndPoints.POST_BY_USER;
+import static io.restassured.RestAssured.given;
+
+public class ApiTest {
+    final String USER_NAME = "autoapi";
+
+    @Test
+    public void getAllPostsByUser() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(POST_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+    }
