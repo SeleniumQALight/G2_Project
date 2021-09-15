79e95e5ef194eb5b2ca61f28469ed86df3e2b3fc
Lana230208 <office@jakera.com.ua>
2021-09-15 22:03:24 +0300

    api1

---
+++ tatsiiProj/src/test/java/apiTest/ApiTest.java
@@ -0,0 +1,54 @@
+package apiTest;
+
+import api.AuthorDTO;
+import api.PostDTO;
+import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static api.EndPoints.POST_BY_USER;
+import static io.restassured.RestAssured.given;
+
+public class ApiTest {
+    final String USER_NAME = "autoapi";
+    Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    public void getAllPostsByUser() {
+        PostDTO[] responseBody = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(POST_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .response().as(PostDTO[].class)
+                ;
+        logger.info(responseBody.length);
+        logger.info(responseBody[0].getTitle());
+        logger.info(responseBody[0].getAuthor().getUsername());
+        for (int i = 0; i < responseBody.length; i++) {
+            Assert.assertEquals("Username", "autoapi", responseBody[i].getAuthor().getUsername());
+        }
+
+        PostDTO[] expectedPostDTO = {
+                new PostDTO("test2", "test body2", "All Users", new AuthorDTO(USER_NAME), false),
+                new PostDTO("test", "test body", "All Users", new AuthorDTO(USER_NAME), false)
+        };
+
+        Assert.assertEquals(responseBody.length, expectedPostDTO.length);
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (int i = 0; i < expectedPostDTO.length; i++) {
+            softAssertions.assertThat(expectedPostDTO[i])
+                    .isEqualToIgnoringGivenFields(responseBody[i], "_id", "createdDate", "author");
+            softAssertions.assertThat(expectedPostDTO[i].getAuthor())
+                    .isEqualToIgnoringGivenFields(responseBody[i].getAuthor(), "avatar");
+        }
+        softAssertions.assertAll();
+    }
+}
