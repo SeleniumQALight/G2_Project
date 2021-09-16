fbc63422edf3884271b04443d08602ec77b3852b
Hanna.Gupalo <gupanka28@gmail.com>
2021-09-16 21:37:16 +0300

    create post test

---
+++ gupaloProj/src/test/java/apiTests/CreatePostByApiTest.java
@@ -0,0 +1,16 @@
+package apiTests;
+
+import org.junit.Test;
+
+
+
+public class CreatePostByApiTest {
+    final String userName = "hanna123";
+    final String password = "qwerty123456";
+
+    @Test
+    public void createNewPostBuApi (){
+
+
+    }
+}
