6a0e9309958a91f4a16ee3c196a784472a4ee685
Victoria <victory.kuts@gmail.com>
2021-09-16 21:36:26 +0300

    create post test

---
+++ kutsProj/src/test/java/apiTests/CreatePostByApiTest.java
@@ -0,0 +1,13 @@
+package apiTests;
+
+import org.junit.Test;
+
+public class CreatePostByApiTest {
+    final String userName = "testovi4";
+    final String passWord = "Q1w2e3r4t5y^";
+
+    @Test
+    public void createNewPostByApi(){
+
+    }
+}
