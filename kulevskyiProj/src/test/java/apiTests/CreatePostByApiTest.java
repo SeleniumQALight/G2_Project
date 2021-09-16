a03575b94762e20c5fbcebb5055843fcb5b5a2b1
Chuck001-creator <kulevskyi@ukr.net>
2021-09-16 21:37:48 +0300

    api tests kulevskyi

---
+++ kulevskyiProj/src/test/java/apiTests/CreatePostByApiTest.java
@@ -0,0 +1,13 @@
+package apiTests;
+
+import org.junit.Test;
+
+public class CreatePostByApiTest {
+    final String userName = "kulevskyi";
+    final String passWord = "deniSKAA9210908383";
+
+    @Test
+    public void createNewPostByApi(){
+
+    }
+}
