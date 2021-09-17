04ef0b25a908c48ca40ba70e4e1d8f80860fb877
qanaha <malvanatalya@ukr.net>
2021-09-17 04:14:46 +0300

    validate schema json

--- halonzaProj/src/test/java/apiTests/apiePrivatBTests/ApiPVBTests.java
+++ halonzaProj/src/test/java/apiTests/apiePrivatBTests/ApiPVBTests.java
@@ -7,10 +7,14 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class ApiPVBTests extends ApiPVBBaseTest{
+
     @Test
     public void getAllCurrencyRate() {
+        final String[] expectedCurrencyArray = {"EUR", "USD", "RUR", "BTC"};
+        final String[] expectedBasicCurrencyArray = {"UAH", "USD"};
         CurrencyDTO[] responseBody = given()
                 .contentType(ContentType.JSON)
                 .queryParam("json")
@@ -24,7 +28,6 @@ public class ApiPVBTests extends ApiPVBBaseTest{
                 .log().all()
                 .extract()
                 .response().as(CurrencyDTO[].class);
-        String[] expectedCurrencyArray = {"EUR", "USD", "RUR", "BTC"};
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(responseBody.length).isEqualTo(expectedCurrencyArray.length);
         for (int i = 0; i < responseBody.length; i++) {
@@ -34,11 +37,32 @@ public class ApiPVBTests extends ApiPVBBaseTest{
                     " и продажи " + responseBody[i].getSale());
             softAssertions.assertThat(responseBody[i].getCcy()).isIn(expectedCurrencyArray);
             if (responseBody[i].getCcy().equalsIgnoreCase(expectedCurrencyArray[3])) {
-                softAssertions.assertThat(responseBody[i].getBase_ccy()).isEqualTo("USD");
+                softAssertions.assertThat(responseBody[i].getBase_ccy())
+                        .as(responseBody[i].getBase_ccy()+ " has incorrect basicCurrency " + expectedBasicCurrencyArray[1])
+                        .isEqualTo(expectedBasicCurrencyArray[1]);
             } else {
-                softAssertions.assertThat(responseBody[i].getBase_ccy()).isEqualTo("UAH");
+                softAssertions.assertThat(responseBody[i].getBase_ccy())
+                        .as(responseBody[i].getBase_ccy()+ " has incorrect basicCurrency " + expectedBasicCurrencyArray[0])
+                        .isEqualTo(expectedBasicCurrencyArray[0]);
             }
         }
         softAssertions.assertAll();
+    }
+
+
+    @Test
+    public void getAllCurrencyRateBySchema() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .queryParam("json")
+                .queryParam("exchange")
+                .queryParam("coursid", "5")
+                .when()
+                .get(EndPoints.privatBankApiExchangeRate)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("responseCashExchangePVB.json"));
     }
 }
