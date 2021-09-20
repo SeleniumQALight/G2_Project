5bb3421e8d966d423460d89276fbd5a8050c5195
stas-kuzub <stas.kuzub@gmail.com>
2021-09-20 12:24:16 -0400

    Kuzub-DZ1-19-9

---
+++ kuzubProj/src/main/java/API/CurrencyAPI.java
@@ -0,0 +1,43 @@
+package API;
+
+import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static API.PrivatEndpoints.*;
+import static io.restassured.RestAssured.given;
+
+public class CurrencyAPI {
+    Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    public void getCurrency() {
+        CurrencyDTO[] response = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+        .when()
+                .get(Currency)
+        .then()
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .response().as(CurrencyDTO[].class);
+
+        CurrencyDTO[] ExpectedCurrencyDTO = {
+            new CurrencyDTO("USD", "UAH"),
+            new CurrencyDTO("EUR", "UAH"),
+            new CurrencyDTO("RUR", "UAH"),
+            new CurrencyDTO("BTC", "USD")
+        };
+
+        Assert.assertEquals(response.length, ExpectedCurrencyDTO.length);
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < ExpectedCurrencyDTO.length; i++) {
+            softAssertions.assertThat(ExpectedCurrencyDTO[i])
+                    .isEqualToIgnoringGivenFields(response[i], "buy", "sale");
+        }
+        softAssertions.assertAll();
+    }
+}
---
+++ kuzubProj/src/main/java/API/PrivatEndpoints.java
@@ -0,0 +1,6 @@
+package API;
+
+public interface PrivatEndpoints {
+    String baseUrl = "https://api.privatbank.ua";
+    String Currency = baseUrl + "/p24api/pubinfo?json&exchange&coursid=5";
+}
