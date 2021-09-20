f96717b958e5ee5ec3e10f81d639facb01837f26
Lana230208 <office@jakera.com.ua>
2021-09-20 20:41:23 +0300

    Homework_PrivatBank

--- tatsiiProj/src/main/java/api/EndPointPrivatBank.java
+++ tatsiiProj/src/main/java/api/EndPointPrivatBank.java
@@ -2,7 +2,6 @@ package api;
 
 public interface EndPointPrivatBank {
     String baseUrl = "https://api.privatbank.ua";
-    String POST_BY_USER = baseUrl + "/p24api/pubinfo?json&exchange&coursid=5/{0}";
-
-    String LOGIN = baseUrl + "/api/login";
+    String GET_API_CURRENCY = baseUrl + "/p24api/pubinfo?json&exchange";
 }
+// protocol://domain/endpoint?get params
---
+++ tatsiiProj/src/test/java/apiTest/ApiPrivatBankTest.java
@@ -0,0 +1,28 @@
+package apiTest;
+
+import api.CurrencyDTO;
+import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import static api.EndPointPrivatBank.GET_API_CURRENCY;
+import static io.restassured.RestAssured.given;
+
+public class ApiPrivatBankTest {
+    Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    public void getAllCurrencies() {
+        CurrencyDTO[] responseBody = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .queryParam("courseId", "5")
+                .when()
+                .get(GET_API_CURRENCY)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .response().as(CurrencyDTO[].class);
+    }
+}
