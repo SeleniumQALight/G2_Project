3e02e885fa5aa7481d2c109d5dfda18c0fd947a9
Lazareva <lirusya@hotmail.com>
2021-09-21 22:26:38 +0300

    CreatePostByApiTest part2

--- lazarevaProj/src/test/java/apiTests/CreatePostByApiTest.java
+++ lazarevaProj/src/test/java/apiTests/CreatePostByApiTest.java
@@ -1,15 +1,73 @@
 package apiTests;
 
+import api.ApiHelper;
+import api.AuthorDTO;
+import api.EndPoints;
+import api.PostDTO;
+import io.restassured.http.ContentType;
+import org.assertj.core.api.SoftAssertions;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class CreatePostByApiTest {
-    final  String userName = "testiren";
-    final String passWord = "qerty12345678";
+    final String userName = "testiren";
+    final String passWord = "qwerty12345678";
+    ApiHelper apiHelper = new ApiHelper();
+
+    @Before
+    public  void  deleteAllPosts(){
+        //login, get all posts, delete
+        apiHelper.deletePostsTillPresent(userName,passWord);
+    }
 
     @Test
+    public  void createNewPOstByApi() {
+        String token =apiHelper.getToken(userName, passWord);
 
-    public  void createNewPOstByApi90 () {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("title", "New post from API");
+        requestParams.put("body","post body");
+        requestParams.put("select1", "OnePerson");
+        requestParams.put("token", token);
+        String response =
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestParams.toMap())
+                .log().all()
+        .when()
+                .post(EndPoints.CREATE_POST)
+        .then()
+                .statusCode(200)
+        .extract().response().getBody().asString();
 
+        Assert.assertEquals("Message", "\"Congrats.\"", response);
+
+        PostDTO[] actualPostDTO = apiHelper.getAllPostsByUser(userName);
+        // verify N-s of  actual posts
+        Assert.assertEquals("Number of posts", 1, actualPostDTO.length);
+        // verify if the Post has correct data
+        PostDTO[] expectedPostDTO = {
+
+                new PostDTO(
+                        requestParams.getString("title"),
+                        requestParams.getString("body"),
+                        requestParams.getString("select1"),
+                        new AuthorDTO(userName),
+                        false
+                )
+        };
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(actualPostDTO[0])
+                .isEqualToIgnoringGivenFields(expectedPostDTO[0], "_id", "createdDate","author");
+        softAssertions.assertThat(actualPostDTO[0].getAuthor())
+                .isEqualToIgnoringGivenFields(expectedPostDTO[0].getAuthor(), "avatar");
+
+        softAssertions.assertAll();
 
     }
 
