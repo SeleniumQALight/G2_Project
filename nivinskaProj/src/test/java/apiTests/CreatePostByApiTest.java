5e60a2db5559e4c44f4ac444586aefd922d127c1
Nivinskaya <nivinskayaa@gmail.com>
2021-09-21 15:24:01 +0300

    nivinskaya api 3 part 2

---
+++ nivinskaProj/src/test/java/apiTests/CreatePostByApiTest.java
@@ -0,0 +1,13 @@
+package apiTests;
+
+import org.junit.Test;
+
+public class CreatePostByApiTest {
+    final String userName = "nivinskao";
+    final String passWord = "caramacara12";
+
+    @Test
+    public void createNewPostByApi(){
+
+    }
+}
