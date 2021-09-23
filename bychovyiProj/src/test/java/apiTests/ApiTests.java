e27f042b7cad5924a41a7b607d238c3dab1f025e
qaStas <qa.gioc@gmail.com>
2021-09-23 21:15:41 +0300

    qaStas api tsts

--- bychovyiProj/src/test/java/apiTests/ApiTests.java
+++ bychovyiProj/src/test/java/apiTests/ApiTests.java
@@ -2,6 +2,7 @@ package apiTests;
 
 import api.AuthorDTO;
 import api.PostDTO;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.log4j.Logger;
@@ -24,10 +25,11 @@ public class ApiTests {
     public void getAllPostsByUser() {
         PostDTO[] responseBody = given()
                 .contentType(ContentType.JSON)
+                .filter(new AllureRestAssured())
                 .log().all()
-                .when()
+        .when()
                 .get(POST_BY_USER, USER_NAME)
-                .then()
+        .then()
                 .statusCode(200)
                 .log().all()
                 .extract()
