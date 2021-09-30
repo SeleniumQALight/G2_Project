90890bdade2af73b4ed39f7e6a923a316b38aa89
naglenko <naglenko@fuib.com>
2021-09-30 23:22:09 +0300

    added  cucumber api+ui tests

---
+++ nahlenkoProj/src/test/java/StepDefinitions/ProfilePage_StepDefinition.java
@@ -0,0 +1,22 @@
+package StepDefinitions;
+
+import cucumber.api.java.en.And;
+import io.cucumber.java.en.Then;
+import pages.ProfilePage;
+
+import static libs.DriverHelper.getWebDriver;
+
+public class ProfilePage_StepDefinition {
+    private ProfilePage profilePage=new ProfilePage(getWebDriver());
+
+    @Then("^User is redirect to Profile page$")
+    public void userIsRedirectToProfilePage(){
+    profilePage.checkIsRedirectToProfilePage();
+        }
+
+    @And("^User sees (\\d+) posts in Posts list on Profile page$")
+    public void userSeesPostsInPostsListOnProfilePage(int expectedNumberOfPosts) {
+    profilePage.checkNumberOfPosts(expectedNumberOfPosts);
+    }
+}
+
