a94a04f2a5c1e0c782a3bb2e32154f1768b3ce4c
Taras Radulenko <Taras_Radulenko@epam.com>
2021-10-01 19:10:49 +0300

    extra files were commented

--- rakhmanovaProj/src/test/java/StepDefinitions/PrivatBank_StepDefinition.java
+++ rakhmanovaProj/src/test/java/StepDefinitions/PrivatBank_StepDefinition.java
@@ -9,19 +9,19 @@ import pages.LoginPage;
 import static libs.DriverHelper.getWebDriver;
 
 public class PrivatBank_StepDefinition {
-    private LoginPage loginPage = new LoginPage(getWebDriver());
-    private HomePage homePage = new HomePage(getWebDriver());
-
-    @Given("^User opens 'Home' page$")
-    public void givenUserOpensHomePage() {
-      loginPage.loginWithValidCred()
-              .checkIsRedirectOnHomePage();
-    }
-
-    @When("^User clicks on 'Profile' button on 'Home' page$")
-    public void userClicksOnProfileButtonOnHomePage() {
-        homePage.clickOnButtonProfile();
-
-    }
+//    private LoginPage loginPage = new LoginPage(getWebDriver());
+//    private HomePage homePage = new HomePage(getWebDriver());
+//
+//    @Given("^User opens 'Home' page$")
+//    public void givenUserOpensHomePage() {
+//      loginPage.loginWithValidCred()
+//              .checkIsRedirectOnHomePage();
+//    }
+//
+//    @When("^User clicks on 'Profile' button on 'Home' page$")
+//    public void userClicksOnProfileButtonOnHomePage() {
+//        homePage.clickOnButtonProfile();
+//
+//    }
 }
 
