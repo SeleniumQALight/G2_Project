e49f240641dacc42572aa5bf1410cef866bafa29
Pavel Canayev <Pavel.Canayev@upc.ua>
2021-10-05 00:27:08 +0300

    HW2(valid login test)

--- kanaievProj/src/test/java/StepDefinitions/LoginPage_StepDefinition.java
+++ kanaievProj/src/test/java/StepDefinitions/LoginPage_StepDefinition.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.When;
 import pages.LoginPage;
 
 import static libs.DriverHelper.getWebDriver;
+import static libs.TestData.*;
 
 public class LoginPage_StepDefinition {
     private LoginPage loginPage = new LoginPage(getWebDriver());
@@ -17,12 +18,20 @@ public class LoginPage_StepDefinition {
 
     @When("^User enters '(.*)' login into 'Login' input on 'Login' page$")
     public void user_enters_Wrong_login_login_into_Login_input_on_Login_page(String userName) {
-        loginPage.enterLoginInSignIn(userName);
+        if ("default".equalsIgnoreCase(userName)) {
+            loginPage.enterLoginInSignIn(PASHA_USER_NAME);
+        } else {
+            loginPage.enterLoginInSignIn(userName);
+        }
     }
 
     @When("^User enters '(.*)' passWord into 'PassWord' input on 'Login' page$")
     public void user_enters_Wrong_pass_passWord_into_PassWord_input_on_Login_page(String password) {
-        loginPage.enterPasswordInSignIn(password);
+        if ("default".equalsIgnoreCase(password)) {
+            loginPage.enterPasswordInSignIn(PASHA_PASSWORD);
+        } else {
+            loginPage.enterPasswordInSignIn(password);
+        }
     }
     @When("^User click on 'SingIn' button on 'Login' page$")
     public void user_click_on_SingIn_button_on_Login_page() {
