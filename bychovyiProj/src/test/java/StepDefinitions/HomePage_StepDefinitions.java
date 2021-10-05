254e8672c75142b95dfcd61f4390b8608037322e
qaStas <qa.gioc@gmail.com>
2021-10-05 09:21:37 +0300

    qaStas R002 Use default userName and passWord

--- bychovyiProj/src/test/java/StepDefinitions/HomePage_StepDefinitions.java
+++ bychovyiProj/src/test/java/StepDefinitions/HomePage_StepDefinitions.java
@@ -1,27 +1,22 @@
 package StepDefinitions;
 
-<<<<<<< HEAD
 import cucumber.api.java.en.Then;
-import pages.HomePage;
-=======
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import pages.HomePage;
 import pages.LoginPage;
->>>>>>> 3c469c28421e914b532ccae11c9a5af1ffa44e56
+
 
 import static libs.DriverHelper.getWebDriver;
 
 public class HomePage_StepDefinitions {
-<<<<<<< HEAD
     private HomePage homePage = new HomePage(getWebDriver());
+    private LoginPage loginPage = new LoginPage(getWebDriver());
 
     @Then("^User redirect on 'Home' page$")
     public void user_redirect_on_Home_page() {
         homePage.checkIsRedirectOnHomePage();
-=======
-    private LoginPage loginPage = new LoginPage(getWebDriver());
-    private HomePage homePage = new HomePage(getWebDriver());
+    }
 
     @Given("^User opens 'Home' page$")
     public void givenUserOpensHomePage() {
@@ -32,6 +27,5 @@ public class HomePage_StepDefinitions {
     @When("^User clicks on 'Profile' button on 'Home' page$")
     public void userClicksOnProfileButtonOnHomePage() {
         homePage.clickOnButtonProfile();
->>>>>>> 3c469c28421e914b532ccae11c9a5af1ffa44e56
     }
 }
--- bychovyiProj/src/test/resources/loginTest/login.feature
+++ bychovyiProj/src/test/resources/loginTest/login.feature
@@ -16,13 +16,11 @@ Feature: User Login
       |             | 123456qwerty |
 
   @R002
-  Scenario Outline: R002 Login with valid Login
+  Scenario: R002 Login with valid Login
     Given User opens 'Login' page
-    When User enters '<login>' login into 'Login' input on 'Login' page
-    And User enters '<passWord>' passWord into 'PassWord' input on 'Login' page
+    When User enters valid login 'default' into 'Login' input on'Login' page
+    And User enters valid passWord 'default' into 'PassWord' input on 'Login' page
     And User click on 'SingIn' button on 'Login' page
     Then User redirect on 'Home' page
 
-    Examples:
-      | login  | passWord     |
-      | qastas | 123456qwerty |
+
